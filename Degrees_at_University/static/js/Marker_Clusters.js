774e0dbfd821851d993e929af86f77bb85a9b1da
dianess <analystdiane@gmail.com>
2020-01-07 13:13:37 -0700

    updates, add to gitignore

--- Degrees_at_University/static/js/Marker_Clusters.js
+++ Degrees_at_University/static/js/Marker_Clusters.js
@@ -12,18 +12,6 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// // Store API query variables
-// var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-// var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
-// var complaint = "&complaint_type=Rodent";
-// var limit = "&$limit=10000";
-
-// // Assemble API query URL
-// var url = baseURL + date + complaint + limit;
-
-// // Grab the data with d3
-// d3.json(url, function(response) {
-
  // Read geoJSON
  d3.json("data/allrankings.geojson", function(collegeData) {  
   var collegeInfo = collegeData.features;
